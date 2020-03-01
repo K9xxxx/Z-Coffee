e0db72ff1738fa41a46c6a8a298bb7b4ecd67871
K9xxxx <patrykjarybssacc@gmail.com>
2020-03-01 12:16:55 +0100

    Making a site fully responsive.

--- main.js
+++ main.js
@@ -4,26 +4,69 @@ const hamburger=document.querySelector(".icofont-navigation-menu");
 const menu=document.querySelector(".menu");
 const calazawartosc=document.querySelector(".calazawartosc");
 const logo=document.querySelector(".logo");
+const ico1=document.getElementById("icd1");
+const ico2=document.getElementById("icd2");
+const ico3=document.getElementById("icd3");
+const box_a_div=document.getElementById("box_a");
+const box_b_div=document.getElementById("box_b");
+const box_c_div=document.getElementById("box_c");
 
-
+// Nie potrzebne ale do zostawienia
 const tl = new TimelineMax();
-
 const toggleMenu= new TimelineMax({paused:true,reversed:true});
+const footMenu= new TimelineMax();
 
-
+// jezeli klasa 'js-x' juz jest to funkcja nic nie zwrcaca
 hamburger.addEventListener('click', () => {
   if (hamburger.classList.contains('js-x')){
     return;}
   });
 
-tl.fromTo(logop,3.5,{opacity:0},{ opacity:0.8})
+
+// Animacja pojawiajacego sie loga
+tl.fromTo(logop,3.5,{opacity:0},{opacity:0.8})
   .fromTo(logoh1,4,{opacity:0},{opacity:0.6},"-=2")
 ;
 
-
 hamburger.addEventListener('click', _ => {
-  hamburger.classList.toggle('js-x');
-  toggleMenu.to(calazawartosc,0.5,{y:125});
-  toggleMenu.to(menu,0.5,{scaleY:1},"-=0.5");
+  hamburger.classList.toggle('js-x'); // dodaje klase 'js-x' do diva
+  toggleMenu.to(calazawartosc,0,{y:125});
+  toggleMenu.to(menu,0,{scaleY:1});
   toggleMenu.reversed() ? toggleMenu.play() : toggleMenu.reverse();
-})
+});
+
+function main(){
+  ico1.addEventListener('click', ()=>{
+    rotation(ico1);
+    sliding(box_a_div);
+  })
+  ico2.addEventListener('click', ()=>{
+    rotation(ico2);
+    sliding(box_b_div);
+  })
+  ico3.addEventListener('click', ()=>{
+    rotation(ico3);
+    sliding(box_c_div);
+  })
+}
+ function sliding(div){
+   if(div.classList.contains("active")){
+     div.classList.remove("active")
+   }
+   else{
+     div.classList.add("active");
+   }
+ }
+
+ function rotation(icofont){
+   if(icofont.classList.contains("rotated")){
+      icofont.classList.remove("rotated");
+      footMenu.to(icofont,0,{rotation:0});
+   }
+   else{
+     icofont.classList.add("rotated");
+     footMenu.to(icofont,0,{rotation:180});
+   }
+ }
+
+ main();
